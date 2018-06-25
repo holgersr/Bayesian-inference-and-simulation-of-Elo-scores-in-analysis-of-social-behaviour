2a0c67188c8c1be98b7ad65de83590cbd9150022
hsr <hsennhenn-reulen@dpz.eu>
2018-06-25 08:41:06 +0200

    Update 00_simulation_main_file_20180625.R

--- 00_simulation_main_file_20180625.R
+++ 00_simulation_main_file_20180625.R
@@ -5,14 +5,14 @@ library("rstan")
 library("viridis")
 library("loo")
 
-source("00b_functions_ml_approach.R")
-source("00b_simulation_functions.R")
+source("00b_functions_ml_approach_20180625.R")
+source("00b_simulation_functions_20180625.R")
 
 today <- format(Sys.Date(), "%Y%m%d")
 
 ## choose (umcomment) one of the following:
-#source("01_simulation_1.R")
-#source("02_simulation_2.R")
-#source("03_simulation_3.R")
-#source("04_simulation_4.R")
-#source("05_simulation_5.R")
+#source("01_simulation_1_20180625.R")
+#source("02_simulation_2_20180625.R")
+#source("03_simulation_3_20180625.R")
+#source("04_simulation_4_20180625.R")
+#source("05_simulation_5_20180625.R")
